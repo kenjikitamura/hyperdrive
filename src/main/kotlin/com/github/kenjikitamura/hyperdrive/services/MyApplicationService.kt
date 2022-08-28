2697a26264bf3bd1a73da1a433401ca8cae4fa90
GitHub Action <action@github.com>
2022-08-28 00:46:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/kenjikitamura/hyperdrive/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kenjikitamura/hyperdrive/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kenjikitamura.hyperdrive.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kenjikitamura.hyperdrive.MyBundle
 
 class MyApplicationService {
 
